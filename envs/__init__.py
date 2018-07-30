20f36b3f3834744622fdca8fe2388528a23f3946
Kytabyte <y69xie@uwaterloo.ca>
2018-07-29 22:43:58 -0400

    patch for #10: multiple updates

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,3 +1,10 @@
 from envs.env2048 import *
+from gym.envs.registration import register
 
 __all__ = ['Env2048']
+
+register(
+    id='2048-v0',
+    entry_point='envs.env2048:Env2048',
+    kwargs={'size': 4}
+)
--- envs/env2048.py
+++ envs/env2048.py
@@ -26,12 +26,12 @@ class Env2048(gym.Env):
     reward = score - self._score
     self._score = score
     return reward
-  
+
   def _is_gameover(self):
     return self._player.is_terminate()
-  
+
   def _info(self):
-    return {'valid_move': self._player.can_move()}
+    return {'valid_move': self._player.can_move(), 'highest': np.max(self._get_obs())}
 
   def n_obs(self):
     return self.observation_space.shape[0] * self.observation_space.shape[1]
@@ -39,14 +39,14 @@ class Env2048(gym.Env):
   def seed(self, seed=None):
     _, seed = seeding.np_random(seed)
     return [seed]
-  
+
   def step(self, action):
     self._player.move(action)
     obs = self._get_obs()
     rwd = self._get_reward()
     done = self._is_gameover()
     info = self._info()
-    
+
     return obs, rwd, done, info
 
   def render(self, mode='human'):
@@ -54,15 +54,15 @@ class Env2048(gym.Env):
     s = str(self._player)
     outfile.write(s)
     return outfile
-  
+
   def reset(self):
     self._player.reset()
     self._score = self._player.score()
     return self._get_obs()
-  
+
 ACTION_MEANING = {
-  0: 'UP',
-  1: 'RIGHT',
-  2: 'DOWN',
-  3: 'LEFT'
+    0: 'UP',
+    1: 'RIGHT',
+    2: 'DOWN',
+    3: 'LEFT'
 }
